995cdd97e571aca994ab0989baf6af5732550fc5
Maria Bychkova <mar.byckova@gmail.com>
2022-10-15 11:45:45 +0300

    Added tests

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,20 +1,21 @@
 package ru.netology.rest;
 
+//import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class PostmanEchoTest {
     @Test
-    void PostRequestTest(){
+    void postRequestTest() {
         given()
-                .baseUri("https://postman-echo.com")
-                //.body("data", name)
+            .baseUri("https://postman-echo.com")
+            .body("postRequestTest")
         .when()
-                .post("/post")
+            .post("/post")
         .then()
-                .statusCode(200)
-                //.body(/* --> ваша проверка здесь <-- */)
-        ;
+            .statusCode(200)
+            .body("data", equalTo("DATA"));
     }
 }
